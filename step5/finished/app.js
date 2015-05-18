c8803de3b2c737244b32c806241c0b2bc3249fe3
Vilmantas Juras <vilmysj@gmail.com>
2015-05-18 22:41:01 +0300

    Add fifth step

---
+++ step5/finished/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+
+app.use('/', express.static('public'));
+
+http.listen(3000);
+
+var users = {},
+    messages = [];
+
+function saveMessage(message) {
+    messages.push(message);
+    if (messages.length > 21) messages.shift();
+};
+
+io.on('connection', function (socket) {
+    console.log('user connected');
+    
+    socket.emit('update users', users);
+    socket.emit('update history', messages);
+
+    socket.on('chat', function(data) {
+        saveMessage(data);
+        socket.broadcast.emit('chat', data);
+    });
+
+    socket.on('new user', function(data) {
+        users[socket.id] = data;
+        io.emit('update users', users);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+
+        delete users[socket.id];
+        io.emit('update users', users);
+    });
+});
